76c679f01c5d6dead2819d8e4789fe060f75d358
Kevin <judekevin007@gmail.com>
2021-03-31 16:50:01 +0530

    env added

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,9 +1,9 @@
 import axios from 'axios';
 
-const url = 'http://localhost:5000/timeSlots';
+const url = process.env.BACKEND_URL || 'http://localhost:4000';
 
-export const getTimeSlots = () => axios.get(url);
-export const getTimeSlot = (id) => axios.get(`${url}/${id}`);
-export const createTimeSlot = (timeSlot) => axios.post(url, timeSlot);
-export const updateTimeSlot = (id, updatedTimeSlot) => axios.patch(`${url}/${id}`, updatedTimeSlot);
-export const deleteTimeSlot = (id) => axios.delete(`${url}/${id}`);
+export const getTimeSlots = () => axios.get(url + '/timeSlots');
+export const getTimeSlot = (id) => axios.get(`${url}/timeSlots/${id}`);
+export const createTimeSlot = (timeSlot) => axios.post(url + '/timeSlots', timeSlot);
+export const updateTimeSlot = (id, updatedTimeSlot) => axios.patch(`${url}/timeSlots/${id}`, updatedTimeSlot);
+export const deleteTimeSlot = (id) => axios.delete(`${url}/timeSlots/${id}`);
--- server/index.js
+++ server/index.js
@@ -3,19 +3,19 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import dotenv from 'dotenv'
+dotenv.config({ path: '../.env' })
 
 import timeSlotsRoutes from './routes/timeSlots.js';
 
 const app = express();
 
-app.use(bodyParser.json({ extended: true }))
-app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 
 app.use('/timeSlots', timeSlotsRoutes);
 
 const CONNECTION_URL = 'mongodb+srv://timeslot-app:timeslot-app@cluster0.0pknp.mongodb.net/timeslot?retryWrites=true&w=majority';
-const PORT = process.env.PORT|| 5000;
+const PORT = process.env.BACKEND_PORT|| 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
